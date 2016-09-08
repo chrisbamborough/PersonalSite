f067b822db2a8c64eef1a8eb701a3cb766ff565d
chris Bamborough <bambarooga@gmail.com>
2016-09-08 16:51:19 +1000

    Add post on 3d perlin terrain.

---
+++ _posts/2016-09-07-P5PerlinLandscape.markdown
@@ -0,0 +1,9 @@
+---
+layout: no-post
+title:  "P5.js Perlin Noise 3D Landscape"
+date:   2016-09-07 19:45:00
+categories: blog
+tags: Creative_Code, p5.js, Perlin_Noise
+image:
+---
+<script src="/js/p5Sketches/P5perlinTerrain.js" type="text/javascript"></script>
--- js/p5Sketches/P5perlinTerrain.js
+++ js/p5Sketches/P5perlinTerrain.js
@@ -1,66 +1,69 @@
-// Daniel Shiffman
+// Based on Daniel Shiffman's Coding challenges in Processing
 // http://codingrainbow.com
 // http://patreon.com/codingrainbow
-// Code for: https://youtu.be/IKB1hWWedMk
 
 var cols, rows;
 var scl = 20;
-var w = 1400;
-var h = 1000;
+var w = 5000;
+var h = 1500;
 
 var flying = 0;
 
 var terrain;
 
+
+var panelParent = document.getElementById('main');
+
+var panel = QuickSettings.create(600, 100, 'Terrain Controls')
+    .addRange("flyingRate", 0.01, 0.5, 0.05, 0.01, draw)
+    .addRange("peak", 10, 250, 75, 1)
+    .addRange("valley", -250, -10, -75, -1);
+
+
+
 function setup() {
-  var canvasDiv = document.getElementById('backgroundCanvas');
-  var width = canvasDiv.offsetWidth;
-  var height = 300;
-  //var height = canvasDiv.offsetHeight;
-  //console.log(canvasDiv);
-  //console.log(width);
-  //console.log(height);
-  var sketchCanvas = createCanvas(width,height, WEBGL);
-  //var sketchCanvas = createCanvas(windowWidth,450);
-  sketchCanvas.parent("backgroundCanvas");
-  console.log(sketchCanvas)
+    var canvasDiv = document.getElementById('backgroundCanvas');
+    var width = canvasDiv.offsetWidth;
+    var height = canvasDiv.offsetHeight;
+    var sketchCanvas = createCanvas(width, height, WEBGL);
+    sketchCanvas.parent("backgroundCanvas");
 
-  cols = w / scl;
-  rows = h/ scl;
+    cols = w / scl;
+    rows = h / scl;
 
-  terrain = [];
-  for (var x = 0; x < cols; x++) {
-    terrain[x] = [];
-  }
+    // make a 2d array
+    terrain = [];
+    for (var x = 0; x < cols; x++) {
+        terrain[x] = [];
+    }
+
+
 }
 
 function draw() {
 
-  flying -= 0.1;
-  var yoff = flying;
-  for (var y = 0; y < rows; y++) {
-    var xoff = 0;
-    for (var x = 0; x < cols; x++) {
-      terrain[x][y] = map(noise(xoff, yoff), 0, 1, -100, 100);
-      xoff += 0.2;
+    flying += panel.getRangeValue('flyingRate');
+    var yoff = flying;
+    for (var y = 0; y < rows; y++) {
+        var xoff = 0;
+        for (var x = 0; x < cols; x++) {
+            terrain[x][y] = map(noise(xoff, yoff), 0, 1, panel.getRangeValue("valley"), panel.getRangeValue("peak"));
+            xoff += 0.2;
+        }
+        yoff += 0.2;
     }
-    yoff += 0.2;
-  }
 
-  background(0);
-  translate(0, 50);
-  rotateX(-PI/3);
-  translate(-w/2, -h/2);
-  for (var y = 0; y < rows-1; y++) {
-    //beginShape(TRIANGLE_STRIP);
-    for (var x = 0; x < cols; x++) {
-      push();
-      translate(x*scl, y*scl, terrain[x][y]);
-      box(scl);
-      pop();
-      //vertex(x*scl, y*scl, terrain[x][y]);
-      //vertex(x*scl, (y+1)*scl, terrain[x][y+1]);
+    rotateX(-PI / 3);
+    translate(-w / 2, height / 2);
+    for (var y = 0; y < rows - 1; y++) {
+        beginShape();
+        stroke('rgba(0,255,0,0.25)')
+        for (var x = 0; x < cols - 1; x++) {
+            vertex(x * scl, y * scl, terrain[x][y]);
+            vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
+            vertex((x + 1) * scl, (y + 1) * scl, terrain[x + 1][y + 1]);
+            vertex((x + 1) * scl, y * scl, terrain[x + 1][y]);
+        }
+        endShape();
     }
-    //endShape();
-  }
 }
