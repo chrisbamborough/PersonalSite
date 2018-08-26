03477b48f852ebe068d8bda53e508b8a85aa2944
chris Bamborough <bambarooga@gmail.com>
2018-08-26 13:27:52 +1000

    Add quicker quad terrain

--- js/p5Sketches/P5perlinTerrain.js
+++ js/p5Sketches/P5perlinTerrain.js
@@ -3,16 +3,15 @@
 // http://patreon.com/codingrainbow
 
 var cols, rows;
-var scl = 20;
+var scl = 30;
 var w;
 var h;
-
 var flying = 0;
 // var noiseInc = 0.2;
 var terrain;
-// var flyingRate = 0.1;
-// var peak = 50;
-// var valley = -50;
+var flyingRate = 0.05;
+var peak = 200;
+var valley = -100;
 
 function setup() {
   var canvasDiv = document.getElementById('backgroundCanvas');
@@ -35,44 +34,57 @@ function setup() {
 
 function draw() {
 
-  flying -= 0.1;
+  flying -= flyingRate;
   var yoff = flying;
   for (var y = 0; y < rows; y++) {
     var xoff = 0;
     for (var x = 0; x < cols; x++) {
-      terrain[x][y] = map(noise(xoff, yoff), 0, 1, -100, 100);
+      // control limit of noise
+      terrain[x][y] = map(noise(xoff, yoff), 0, 1, peak, valley);
+      //map(value,start1,stop1,start2,stop2,[withinBounds])
       xoff += 0.2;
     }
     yoff += 0.2;
   }
 
   background(255);
-  translate(0, height/10);
+  translate(0, height / 5);
   rotateX(PI / 3);
   translate(-w / 2, -h / 2);
 
-
+  // using quads
   for (var y = 0; y < (rows - 1); y++) {
-    beginShape(TRIANGLES);
+    beginShape(QUADS);
     strokeWeight(0.5);
-    stroke('rgba(0,0,0,255)');
-    // fill(255);
-    noFill()
+    stroke(0);
+    //fill(255);
+    noFill();
     for (var x = 0; x < cols - 1; x++) {
-      // vertex(x * scl, y * scl, terrain[x][y]);
-      // vertex((x + 1) * scl, y * scl, terrain[(x + 1)][y]);
-      // vertex((x + 1) * scl, (y + 1) * scl, terrain[(x + 1)][(y + 1)]);
-      // vertex(x * scl, (y + 1) * scl, terrain[x][(y + 1)]);
-
       vertex(x * scl, y * scl, terrain[x][y]);
       vertex(x * scl, (y + 1) * scl, terrain[x][(y + 1)]);
-      vertex((x + 1) * scl, y * scl, terrain[(x + 1)][y]);
-      vertex(x * scl, (y + 1) * scl, terrain[x][(y + 1)]);
       vertex((x + 1) * scl, (y + 1) * scl, terrain[(x + 1)][(y + 1)]);
-
+      vertex((x + 1) * scl, y * scl, terrain[(x + 1)][y]);
     }
     endShape();
   }
+
+
+
+  // for (var y = 0; y < (rows - 1); y++) {
+  //   beginShape(TRIANGLES);
+  //   strokeWeight(0.5);
+  //   stroke(0);
+  //   //fill(255);
+  //   noFill();
+  //   for (var x = 0; x < cols - 1; x++) {
+  //     vertex(x * scl, y * scl, terrain[x][y]);
+  //     vertex(x * scl, (y + 1) * scl, terrain[x][(y + 1)]);
+  //     vertex((x + 1) * scl, y * scl, terrain[(x + 1)][y]);
+  //     vertex(x * scl, (y + 1) * scl, terrain[x][(y + 1)]);
+  //     vertex((x + 1) * scl, (y + 1) * scl, terrain[(x + 1)][(y + 1)]);
+  //   }
+  //   endShape();
+  // }
 }
 
 function windowResized() {
